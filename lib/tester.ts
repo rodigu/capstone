774cb82334deb31c5e3d8572a2141d12675111b0
Rodrigo Morais <58960796+rodigu@users.noreply.github.com>
2021-06-10 22:27:42 -0300

    📁Minor comments

--- lib/tester.ts
+++ lib/tester.ts
@@ -1,10 +1,10 @@
 import { Network } from './network.js'
 
 const net:Network = new Network();
-
+// Adding Nodes
 net.addNode({ id: 'A' });
 net.addNode({ id: 'B' });
-
+// Adding Edges
 net.addEdge({ id: 'coolEdge', nodeA: 'A', nodeB: 'B' })
-
+// Checking ou net
 console.log(net)
