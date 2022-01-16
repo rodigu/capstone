5568762a745dcf8f86d45c17f08d6b3e23b1e78a
Rodrigo Morais <58960796+rodigu@users.noreply.github.com>
2022-01-16 03:38:37 -0300

    ⚙️Updated testing

--- lib/tester.ts
+++ lib/tester.ts
@@ -1,24 +1,7 @@
-import { Network } from './network.js';
+import * as nets from './ne.ts';
 
-import * as algo from './algorithms.js';
+const net = new nets.Network();
 
-function testNet (network:Network, title:string) {
-  console.log(`--- ${title} ---`);
+net.addEdge({vertice_a:1,vertice_b:2});
 
-  console.log('Network:','\n  nodes:', network.nodes, '\n  edges:', network.edges);
-
-  console.log(`Network genus: ${algo.genus(network)}`);
-
-  console.log(`Network weight: ${algo.networkWeight(network)}`);
-
-  console.log(`Out-neighbors for node 1:`, algo.outNeighbors(network, 1) ?? 'network is undirected');
-}
-
-const number_nodes = 10;
-const number_edges = 11;
-
-const net_undirected:Network = algo.randomNetworkGen(number_nodes, number_edges);
-testNet(net_undirected, 'Undirected Network');
-
-const net_directed:Network = algo.randomNetworkGen(number_nodes, number_edges, true);
-testNet(net_directed, 'Directed Network');
+console.log(net);
