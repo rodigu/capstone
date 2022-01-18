1a7e4fb3d323cd75406aa0b6ef053d8dfc9a3665
Rodrigo Morais <58960796+rodigu@users.noreply.github.com>
2022-01-18 16:08:13 -0300

    📁Added documentation to edge.ts - it also uses enums now

--- lib/edge.ts
+++ lib/edge.ts
@@ -1,15 +1,30 @@
-export class Edge {
-  private vertice_b: number | string;
-  private vertice_a: number | string;
-  weight: number;
+import { base_id, EdgeArgs } from './enums.ts';
 
-  constructor (args: { vertice_a: number | string, vertice_b: number | string, weight?: number }) {
+export class Edge {
+  private vertice_b:base_id;
+  private vertice_a:base_id;
+  weight:number;
+
+  /**
+   * ### Edge constructor
+   * Create an edge between `vertice_a` and `vertice_b`.
+   * Weight is set to 1 by default (i.e. unweighted).
+   * 
+   * @param  {EdgeArgs} args
+   */
+  constructor (args:EdgeArgs) {
     this.vertice_a = args.vertice_a;
     this.vertice_b = args.vertice_b;
     this.weight ??= 1;
   }
 
-  get vertices () : { a: number | string, b: number | string } {
+  /**
+   * ### Vertices getter
+   * Returns an object with the two vertices in the egde.
+   * 
+   * @returns {{ a:base_id, b:base_id }}
+   */
+  get vertices () : { a:base_id, b:base_id } {
     return { a: this.vertice_a, b: this.vertice_b };
   }
 }
