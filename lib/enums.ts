4ae01c37e69c638ebb84876b95330098fdf7a5db
Rodrigo Morais <58960796+rodigu@users.noreply.github.com>
2022-01-18 16:22:36 -0300

    📁Moved ERROR to enum.ts

--- lib/enums.ts
+++ lib/enums.ts
@@ -19,3 +19,14 @@ export interface NetworkArgs {
     edge_limit?:number;
     vertice_limit?:number;
 }
+
+export const ERROR = {
+  UNDEFINED_VALUES: "Undefined values being given as arguments!",
+  EDGE_LIMIT: "Can't add new edge. Limit of Edges exceeded",
+  VERTICE_LIMIT: "Can't add new vertice. Limit of Vertices exceeded",
+  EXISTING_EDGE: "Trying to add an edge with already existing ID",
+  EXISTING_VERTICE: "Trying to add a vertice with already existing ID",
+  INEXISTENT_VERTICE: "Vertice doesn't exist",
+  NOT_MULTIGRAPH: "Trying to add multiple edges between two vertices. Graph is not a multigraph!",
+  UNDEFINED_ID: "Tried to use undefined id as input",
+};
