9b2aa85d5078bd61b77c27be6054ab9034f1aa44
Rodrigo Morais <58960796+rodigu@users.noreply.github.com>
2022-01-18 20:49:26 -0300

    🪒Moved neighborhood functions to network class

--- lib/algorithms.ts
+++ lib/algorithms.ts
@@ -2,58 +2,7 @@ import { Network } from "./network.ts";
 import { base_id } from "./enums.ts";
 
 /**
- * ### Network weight
- * Function to get network's weight
- * 
- * @param  {Network} network
- * @returns number
- */
-export function networkWeight (network:Network) : number {
-    let weight = 0;
-    const { vertices } = network;
-
-    vertices.forEach(vertice => {
-      weight += vertice.weight;
-    });
-
-    return weight;
-}
-
-/**
- * ### Genus
- * Function to get network's [genus](https://en.wikipedia.org/wiki/Genus_%28mathematics%29)
- * 
- * @param  {Network} network
- * @returns number
- */
-export function genus (network:Network) : number {
-  return network.edges.size - network.vertices.size + 1;
-}
-
-/**
- * ### Out neighbors
- * Get out-neighbors of a given vertice in a network
- * 
- * @param  {Network} network
- * @param  {base_id} id
- * @returns base_id[]
- */
-export function outNeighbors (network:Network, id:base_id) : base_id[] | null {
-  if (!network.is_directed) return null;
-
-  const { vertices } = network;
-  const out_neighbors:base_id[] = [];
-  vertices.forEach(vertice => {
-    if (network.hasEdge(id, vertice.id)) out_neighbors.push(vertice.id);
-  });
-
-  return out_neighbors;
-}
-
-/**
- * ### Randomly generate network
- * Tries to generate a network with the given number of nodes and edges
- * 
+ * Tries to generate a network with the given number of nodes and edges.
  * @param  {Object} args
  * @param  {number} args.number_vertices
  * @param  {number} args.number_edges
@@ -68,9 +17,9 @@ export function randomNetworkGen (args: { number_vertices:number, number_edges:n
   for (let vertice = 0; vertice < number_vertices; vertice++)
     net.addVertice({ id: vertice });
   while (net.edges.size < number_edges) {
-    const vertice_a = Math.floor(Math.random() * number_vertices);
-    const vertice_b = Math.floor(Math.random() * number_vertices);
-    net.addEdge({ vertice_a, vertice_b });
+    const from = Math.floor(Math.random() * number_vertices);
+    const to = Math.floor(Math.random() * number_vertices);
+    net.addEdge({ from, to });
   }
 
   return net;
