41d24fec7b42697fc20f1a1fb42de2ee0546ea40
Rodrigo Morais <58960796+rodigu@users.noreply.github.com>
2022-01-20 15:34:58 -0300

    🐛Uncaught #<Object> - stack overflow because it was possible for loop to never end

--- lib/algorithms.ts
+++ lib/algorithms.ts
@@ -8,17 +8,24 @@ import { Network } from "./network.ts";
  * @param  {boolean} [args.is_directed]
  * @returns Network
  */
-export function randomNetworkGen (args: { number_vertices:number, number_edges:number, is_directed?:boolean }) : Network {
+export function randomNetworkGen (args: { number_vertices:number, number_edges:number, is_directed?:boolean, edge_tries?:number }) : Network {
   let { number_vertices, number_edges, is_directed } = args;
   is_directed ??= false;
   const net = new Network({ is_directed });
 
   for (let vertex = 0; vertex < number_vertices; vertex++)
     net.addVertex({ id: vertex });
-  while (net.edges.size < number_edges) {
+
+  args.edge_tries ??= 20;
+  while (net.edges.size < number_edges && args.edge_tries > 0) {
     const from = Math.floor(Math.random() * number_vertices);
     const to = Math.floor(Math.random() * number_vertices);
-    net.addEdge({ from, to });
+    try {
+      net.addEdge({ from, to, do_force: false });
+    } catch (e) {
+      args.edge_tries--;
+      console.log(e.message);
+    }
   }
 
   return net;
