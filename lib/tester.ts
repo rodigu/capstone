c63b7a111629f8fe1a3123b93b07a841f480519d
Rodrigo Morais <58960796+rodigu@users.noreply.github.com>
2022-01-23 21:46:07 -0300

    ⚙️

--- lib/tester.ts
+++ lib/tester.ts
@@ -1,35 +1,51 @@
 import * as nets from "./ne.ts";
 
-console.log("---Starting Tests---");
-
 function logNetwork(network: nets.Network) {
-  console.log(network.vertex_list);
-  console.log(network.edge_list);
+  return (
+    "\n" +
+    JSON.stringify(network.vertex_list) +
+    "\n" +
+    JSON.stringify(network.edge_list) +
+    "\n"
+  );
 }
 
 function valuesTest(network: nets.Network) {
-  console.log(
+  return (
+    "--Values Test--\n" +
     `Weight:                ${network.weight}\n` +
-      `Genus:                 ${network.genus}\n` +
-      `Clique Size:           ${network.max_edges}\n` +
-      `Density:               ${network.density}\n` +
-      `Clustering for ETH:    ${network.clustering("ETH")}\n`
+    `Genus:                 ${network.genus}\n` +
+    `Clique Size:           ${network.max_edges}\n` +
+    `Density:               ${network.density}\n` +
+    `Clustering for ETH:    ${network.clustering("ETH")}\n`
   );
 }
 
 function algorithmTest(network: nets.Network) {
+  let test_string = "--Algorithms Tests--\n";
+
   const k_core = 10;
-  console.log(`${k_core} core-decomposition:\n`);
   const k10 = network.core(k_core);
-  logNetwork(k10);
-  console.log(
-    `${k_core} core-decomposition vertice number: ${k10.vertices.size}\n`
-  );
-  console.log(`${k_core} core-decomposition edge number: ${k10.edges.size}\n`);
+  test_string += `${k_core}-core decomposition vertice number: ${k10.vertices.size}\n`;
+  test_string += `${k_core}-core decomposition edge number: ${k10.edges.size}`;
+
+  return test_string;
 }
 
+function getTestTime(): string {
+  const date = new Date();
+  return (
+    date.getDate() +
+    "_" +
+    date.getMonth() +
+    "_" +
+    date.getFullYear() +
+    "_" +
+    date.getHours()
+  );
+}
 const net_csv = await nets.loadAdjacencyMatrix("./data/networkMatrix.csv");
-console.log("CSV loaded");
-logNetwork(net_csv);
-valuesTest(net_csv);
-algorithmTest(net_csv);
+
+const test_data = valuesTest(net_csv) + "\n" + algorithmTest(net_csv);
+
+Deno.writeTextFile(`./data/test_${getTestTime()}.txt`, test_data);
